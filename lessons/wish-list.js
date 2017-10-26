74817ec22ade38a77e0c2bbb9cae8d9dbbffda87
Tom Wilson <tom@jackhq.com>
2017-10-25 20:42:35 -0400

    make wish list simulation trigger change event

--- lessons/wish-list.js
+++ lessons/wish-list.js
@@ -83,17 +83,19 @@ function verify(args, cb) {
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
     await page.goto('http://localhost:8080')
-    const output = await page.evaluate(() => {
-      var element = document.querySelector('input')
-      element.value = 'FooBar'
-      var event = new Event('change', { bubbles: true })
-      event.simulated = true
-      element.dispatchEvent(event)
+    const output = await page
+      .evaluate(() => {
+        var element = document.querySelector('input')
+        element.value = 'FooBar'
+        var event = new Event('change', { bubbles: true })
+        event.simulated = true
+        element.dispatchEvent(event)
 
-      document.querySelector('button#add-wish').click()
+        document.querySelector('button#add-wish').click()
 
-      return document.querySelector('ul').children
-    })
+        return document.querySelector('ul').children.length
+      })
+      .catch(err => 0)
     if (output === 1) {
       cb(true)
     } else {
